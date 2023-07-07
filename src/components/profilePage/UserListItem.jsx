0e7f52c49d81d1259aacc655604fda49d443ed0a
hungry samurai <hello@hungrysamurai.ru>
2023-07-07 11:51:52 +0300

    profile page refactor

---
+++ src/components/profilePage/UserListItem.jsx
@@ -0,0 +1,23 @@
+import EditListIcon from "./icons/EditListIcon";
+import DeleteListIcon from "./icons/DeleteListIcon";
+
+const UserListItem = ({title, active}) => {
+ return (        
+ <div className={`user-list-container ${active && 'active'}`}>
+    <div className="user-list-title">
+      <h3>{title}</h3>
+    </div>
+
+      <div className="user-list-icons-container">
+        <div className="edit-list-icon">
+         <EditListIcon/>
+        </div>
+        <div className="delete-list-icon">
+         <DeleteListIcon/>
+        </div>
+      </div>   
+  </div>
+ )
+}
+
+export default UserListItem
---
+++ src/components/profilePage/icons/UserListIcon.jsx
@@ -0,0 +1,10 @@
+const UserListIcon = () => {
+ return (
+   <svg width="20" height="28" viewBox="0 0 20 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M15 27.5V7.5C15 6.12125 13.8787 5 12.5 5H2.5C1.12125 5 0 6.12125 0 7.5V27.5L7.5 22.9987L15 27.5Z" fill="var(--primary-color)"/>
+        <path d="M13.2325 2.5H15C16.3787 2.5 17.5 3.62125 17.5 5V18.0537L20 21.165V2.5C20 1.12125 18.8787 0 17.5 0H7.5C6.12125 0 5 1.12125 5 2.5H13.2325Z" fill="var(--primary-color)"/>
+        </svg>
+ )
+}
+
+export default UserListIcon;
