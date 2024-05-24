2e240cf00bce425189f666ecd6ce9e533aeb7d7c
hungry samurai <hello@hungrysamurai.ru>
2024-05-24 11:48:58 +0300

    persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import { Provider } from 'react-redux';
+
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import authReducer from './slices/authSlice';
 import mainReducer from './slices/mainSlice';
@@ -7,19 +19,48 @@ import searchPageParamsReducer from './slices/searchPageParamsSlice';
 import userListsReducer from './slices/userListsSlice';
 import { apiSlice } from './slices/api/apiSlice';
 
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["main", "auth", "homePageParams", "searchPageParams", "userLists", apiSlice.reducerPath],
+};
+
+const mainPersistConfig = {
+  key: "main",
+  storage: storage,
+  whitelist: ["lang"],
+};
+
+const homePageParamsPersistConfig = {
+  key: "homePageParams",
+  storage: storage,
+  whitelist: ["mediaType", "filterList"]
+}
+
+const rootReducer = combineReducers({
+  main: persistReducer(mainPersistConfig, mainReducer),
+  auth: authReducer,
+  homePageParams: persistReducer(homePageParamsPersistConfig, homePageParamsReducer),
+  searchPageParams: searchPageParamsReducer,
+  userLists: userListsReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    main: mainReducer,
-    auth: authReducer,
-    homePageParams: homePageParamsReducer,
-    searchPageParams: searchPageParamsReducer,
-    userLists: userListsReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
   devTools: import.meta.env.DEV ? true : false,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
